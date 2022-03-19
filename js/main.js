311aa99475abd59b6a680876a23cb4d59c8417ae
Marco De La Flor <nerado.one@gmail.com>
2022-03-19 17:59:14 -0300

    commentBox on Article Page implemented

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ const acceder = () => {
         let usuario = prompt('Ingrese Usuario:');
         let contraseña = prompt('Ingrese Contraseña:');
         if (usuario == user.usuario && contraseña == user.contraseña) {
-            locked = false;
+            const locked = false;
             alert('Bienvenido, ha ingresado bajo los permisos de Administrador')
         } else {
             alert('Recuerde que usuario y contraseña por defecto son: admin/admin')
