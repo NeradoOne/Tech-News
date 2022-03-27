bfbd79289f1cd118d45dd9df689782a0b7ac68e4
Marco De La Flor <nerado.one@gmail.com>
2022-03-27 00:54:15 -0300

    loging implemented w/local and session storage

---
+++ js/comments.js
@@ -0,0 +1,44 @@
+//Comentrarios de ejemplo
+const comments = [
+    {name: 'Willy87', comment: 'Lookslike a original Gameboy!'},
+    {name: 'Nate', comment: 'I’m glad this turned out well. I haven’t seen done in person yet, but I got into the GBA modding scene a couple months ago and it’s nuts how good those old games look on a modern screen. Also, if anyone is curious, modding a new screen and even USB-C charging on a GBS SP with a new metal shell is pretty easy if the Pocket is hard to get or out of your price range. (It’s also just a fun project.)'},
+    {name: 'PhilSwn', comment: 'This article is begging for photos/video of GBC and GBA games running on it.'},
+    {name: 'Mattopotamus', comment: 'There is something really nice about old school gaming with just A+B and a directional pad. I can play games like Turtles in Time and Contra for hours.'}
+]
+//Funcion que carga los comentarios
+const loadComments = () => {
+    //Reestablece la lista de comentarios
+    document.querySelector('#comments').innerHTML = '';
+    //Recorre el array de comentarios
+    for (const comment in comments) {
+        //Uso de template para insertar los comentarios en el html
+        let comentario = `
+            <h4 class='comments-name'>${comments[comment].name}</h4>
+            <p  class='comments-comment'>${comments[comment].comment}</p>
+            `;
+        document.querySelector('#comments').innerHTML += comentario;
+
+    }
+    const commentsCounter = document.querySelector('#commentsCounter');
+    commentsCounter.textContent = comments.length;
+}
+loadComments()
+
+const newComment = e => {
+    e.preventDefault();
+// Compueba si el cmpo de comentarios esta vacio
+    if (commentBox.value == ''){
+        alert('Please type your comment')
+    } else {
+        //En caso que este el comentario se envie sin nombre, se le asigna la etiqueta de anonimo al mismo
+            comment = {name: commentName.value == '' ? 'anonymous' : commentName.value, comment: commentBox.value}
+            comments.push(comment)
+            commentName.value = '';
+            commentBox.value = '';
+    loadComments()
+}
+}
+const commentBox = document.querySelector('#commentBox')
+const commentName = document.querySelector('#commentName')
+const btnCreatre = document.querySelector('#btnCreateComment');
+btnCreatre.addEventListener('click', newComment)
--- js/main.js
+++ js/main.js
@@ -1,67 +1,101 @@
 
-const locked = true;
-const user = {
-    usuario: 'admin',
-    contraseña: 'admin'
+const floating = document.querySelector('#floating')
+
+const templates = {
+    none:'',
+    login: `
+    <div class='blured'>
+        <aside class="popUp">
+            <h2>Please login to keep reading</h2>
+            <form class="subscribe-form">
+                <label for="mail">Email:</label>
+                <input required type="email" name="mail" id="mail" placeholder="yourmail@example.com" />
+                <label for="mail">Password:</label>
+                <input required type="password" name="password" id="password" placeholder="Password" />
+                <input class="btn submit" type="submit" value="Login" />
+            </form>
+        </aside>
+    </div>`,
+    register: `
+    <div class='blured'>
+        <aside class="popUp">
+            <h2>Please complete the following form to keep reading</h2>
+            <form class="subscribe-form" method="post">
+                <label for="name">Name:</label>
+                <input required="required" type="text" name="name" id="name" placeholder="Type your name" />
+                <label for="birth">Birth Date:</label>
+                <input type="date" name="birth" id="birth" />
+                <label for="mail">Email:</label>
+                <input required type="email" name="mail" id="mail" placeholder="yourmail@example.com" />
+                <label for="mail">Password:</label>
+                <input required type="password" name="password" id="password" placeholder="Password" />
+                <div class="tyc">
+                    <label for="terms">Do you agree with our <a href="#">terms and conditions</a>?</label>
+                    <input required type="checkbox" name="terms" id="terms" />
+                </div>
+                <input class="btn submit" type="submit" value="Subscribe" />
+                <input class="btn reset" type="reset" value="Reset" />
+            </form>
+        </aside>
+    </div>`
 }
-const acceder = () => {
-    if (locked) { 
-        let usuario = prompt('Ingrese Usuario:');
-        let contraseña = prompt('Ingrese Contraseña:');
-        if (usuario == user.usuario && contraseña == user.contraseña) {
-            const locked = false;
-            alert('Bienvenido, ha ingresado bajo los permisos de Administrador')
+
+const render = template => document.querySelector('#floating').innerHTML = templates[template];
+
+const register = e => {
+    localStorage.setItem('usuario',
+        JSON.stringify({
+            name: e.path[1][0].value ,
+            fecha: e.path[1][1].value,
+            mail: e.path[1][2].value.toLowerCase() ,
+            pass: e.path[1][3].value,
+            terms: e.path[1][4].checked
+            })
+        )
+        init()
+}
+
+const login = e => {
+    e.preventDefault()
+    let mailIngresed = e.path[1][0].value.toLowerCase();
+    let mailStored = JSON.parse(localStorage.getItem('usuario')).mail
+    let passIngresed = e.path[1][1].value;
+    let passStored = JSON.parse(localStorage.getItem('usuario')).pass
+    if (mailIngresed == mailStored) {
+        if (passIngresed == passStored){
+        sessionStorage.setItem('locked', false)
+        init()
         } else {
-            alert('Recuerde que usuario y contraseña por defecto son: admin/admin')
-            acceder()
-        }   
-    }
-}
-acceder()
-
-//Comentrarios de ejemplo
-const comments = [
-    {name: 'Willy87', comment: 'Lookslike a original Gameboy!'},
-    {name: 'Nate', comment: 'I’m glad this turned out well. I haven’t seen done in person yet, but I got into the GBA modding scene a couple months ago and it’s nuts how good those old games look on a modern screen. Also, if anyone is curious, modding a new screen and even USB-C charging on a GBS SP with a new metal shell is pretty easy if the Pocket is hard to get or out of your price range. (It’s also just a fun project.)'},
-    {name: 'PhilSwn', comment: 'This article is begging for photos/video of GBC and GBA games running on it.'},
-    {name: 'Mattopotamus', comment: 'There is something really nice about old school gaming with just A+B and a directional pad. I can play games like Turtles in Time and Contra for hours.'}
-]
-//Funcion que carga los comentarios
-const loadComments = () => {
-    //Reestablece la lista de comentarios
-    document.querySelector('#comments').innerHTML = '';
-    //Recorre el array de comentarios
-    for (const comment in comments) {
-        //Uso de template para insertar los comentarios en el html
-        let comentario = `
-            <h4 class='comments-name'>${comments[comment].name}</h4>
-            <p  class='comments-comment'>${comments[comment].comment}</p>
-            `;
-        document.querySelector('#comments').innerHTML += comentario;
-
-    }
-    const commentsCounter = document.querySelector('#commentsCounter');
-    commentsCounter.textContent = comments.length;
-}
-loadComments()
-
-
-const newComment = e => {
-    e.preventDefault();
-// Compueba si el cmpo de comentarios esta vacio
-    if (commentBox.value == ''){
-        alert('Please type your comment')
+            alert('Wrong password entry')
+        }
     } else {
-        //En caso que este el comentario se envie sin nombre, se le asigna la etiqueta de anonimo al mismo
-            comment = {name: commentName.value == '' ? 'anonymous' : commentName.value, comment: commentBox.value}
-            comments.push(comment)
-            commentName.value = '';
-            commentBox.value = '';
-    loadComments()
+        alert('Wrong mail entry')
+    }
 }
-}
-const commentBox = document.querySelector('#commentBox')
-const commentName = document.querySelector('#commentName')
-const btnCreatre = document.querySelector('#btnCreateComment');
-btnCreatre.addEventListener('click', newComment)
 
+const handleUser = e => {
+    e.preventDefault()
+    if (localStorage.getItem('usuario')) {
+        login(e)
+    } else {
+        register(e)
+    }
+}
+
+function init() {
+    if (sessionStorage.getItem('locked') == 'false') {
+        console.log('Hola'+ JSON.parse(localStorage.getItem('usuario')).name)
+        document.querySelector('#floating').classList.toggle("none")
+        render('none')
+    } else {
+        if (localStorage.getItem('usuario')) {
+            render('login')
+            document.querySelector('.submit').addEventListener('click', handleUser)
+            
+        } else {
+            render('register')
+            document.querySelector('.submit').addEventListener('click', handleUser)
+        }
+    }
+}
+init()
